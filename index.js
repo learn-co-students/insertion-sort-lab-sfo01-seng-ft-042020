687d78ecd03d1d1d81f78d0b41a6d7c8e07d806e
Seanbon0611 <seand0611@gmail.com>
2020-08-03 17:34:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function findMinAndRemove(array){
-
+  //find min in array
+  //remove min from the array and return it
+  let sorted = array.sort((a,b) => a - b);
+  return sorted.shift();
 }
 
 function selectionSort(array){
-
+  //run findMinAndRemove function to get lowest in the array
+  //push that number into the new sorted array
+  //return sorted array
+  let sortedArray = [];
+  let newMin;
+  while (array.length !== 0) {
+    newMin = findMinAndRemove(array)
+    sortedArray.push(newMin)
+  }
+  return sortedArray
 }
