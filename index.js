7b99bbc295a9fd45f40bb63e029bd7298c78456d
Don Mallory <dmm333@gmail.com>
2020-08-18 13:25:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+    let minIndex = 0;
+    let min = array[minIndex];
+    for (let x = 1; x < array.length; x++) {
+        if (array[x] < min) {
+            min = array[x];
+            minIndex = x;
+        }
+    }
+    array.splice(minIndex, 1)
+    return min
 }
 
 function selectionSort(array){
-
+    let result = [];
+    while (array.length > 0) {
+        result.push(findMinAndRemove(array))
+    }
+    return result
 }
